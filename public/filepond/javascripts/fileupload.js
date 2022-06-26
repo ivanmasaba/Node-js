7c07dd29a142b814df0d4c7abb4a2c5666bc1629
ivanmasaba <ivanmasaba@gmail.com>
2022-06-26 15:47:18 +0300

    Finished Project

--- public/filepond/javascripts/fileupload.js
+++ public/filepond/javascripts/fileupload.js
@@ -1,14 +1,33 @@
-// Register the plugin
+const rootStyles = window.getComputedStyle( document.documentElement )
+
+if( rootStyles.getPropertyValue( '--book-cover-width-large' ) != null &&
+   rootStyles.getPropertyValue( '--book-cover-width-large' ) !== '' ){
+  ready()
+}else{
+  document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready(){
+
+  const coverWidth = parseFloat( rootStyles.getPropertyValue( '--book-cover-width-large' ))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue( '--book-cover-aspect-ratio' ))
+  const coverHeight = coverWidth / coverAspectRatio
+
+  // Register the plugin
 FilePond.registerPlugin(FilePondPluginImagePreview);
 FilePond.registerPlugin(FilePondPluginImageResize);
 FilePond.registerPlugin(FilePondPluginFileEncode);
 
 // set aspect ratio
 FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetHeight: 150,
-    imageResizeTargetWidth: 100
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetHeight: coverHeight,
+    imageResizeTargetWidth: coverWidth
 })
 
 // Turn all file input elements into ponds 
   FilePond.parse(document.body);
+
+
+}
+
